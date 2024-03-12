dec20cebd4a35febd861ed72d5c661a4e7ab9a2e
Akademija <akademija@MacBook-Pro-od-Akademija.local>
2024-03-12 21:44:18 +0100

    1

--- SofascoreAcademy/SofascoreAcademy/ViewController.swift
+++ SofascoreAcademy/SofascoreAcademy/ViewController.swift
@@ -6,12 +6,26 @@
 //
 
 import UIKit
+import Foundation
+import SnapKit
 
 class ViewController: UIViewController {
 
+    let leagueView: LeagueView = .init()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+                
+        view.addSubview(leagueView)
+        
+        leagueView.snp.makeConstraints() {
+            $0.top.equalTo(view.safeAreaLayoutGuide.snp.top).inset(16)
+            $0.leading.trailing.equalToSuperview() // Match leading and trailing edges of parent
+
+        }
     }
 }
 
