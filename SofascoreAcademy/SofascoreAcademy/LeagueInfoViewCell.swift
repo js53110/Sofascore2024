147c40532f339ed3e43d36adec35aed78d491190
Akademija <akademija@MacBook-Pro-od-Akademija.local>
2024-03-27 14:37:05 +0100

    ident fix

--- SofascoreAcademy/SofascoreAcademy/LeagueInfoViewCell.swift
+++ SofascoreAcademy/SofascoreAcademy/LeagueInfoViewCell.swift
@@ -5,14 +5,14 @@ class LeagueInfoViewCell: UITableViewCell {
     let leagueInfoView = LeagueInfoView()
 
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
-            super.init(style: style, reuseIdentifier: reuseIdentifier)
-            addViews()
-            setupConstraints()
-        }
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        addViews()
+        setupConstraints()
+    }
     
     required init?(coder: NSCoder) {
-            fatalError("init(coder:) has not been implemented")
-        }
+        fatalError("init(coder:) has not been implemented")
+    }
     
     func update(countryName: String, leagueName: String, leagueLogo: String) {
         leagueInfoView.update(countryName: countryName, leagueName: leagueName, leagueLogo: leagueLogo)
